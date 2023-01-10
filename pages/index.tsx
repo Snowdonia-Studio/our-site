a86e3f7c864c54af4404de6965fa18a634087519
jeremy grice <jeremy.grice@newbalance.com>
2023-01-09 20:43:24 -0500

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,55 +19,9 @@ export default function Home() {
           <p className={styles.navbartext}>
             Snowdonia Studio
           </p>
-
         </div>
-
         <div className={styles.center}>
-          Coming soon!
-        </div>
-
-        <div className={styles.center}>
-          <a
-            href="/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Our Work <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              ...
-            </p>
-          </a>
-
-          <a
-            href="/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              About Us <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              ...
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Work with us <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              ...
-            </p>
-          </a>
+          Our Website is coming soon!
         </div>
       </main>
     </>
