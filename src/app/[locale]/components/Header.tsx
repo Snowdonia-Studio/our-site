4303d5fd7a18d3383523ce91185024c6d69c2c3a
Jeremy <jeremylgrice@gmail.com>
2024-08-02 21:17:41 -0400

    update template used by site

---
+++ src/app/[locale]/components/Header.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { Link } from '@/src/navigation'
+import { useTranslations } from 'next-intl'
+import { FC } from 'react'
+import Image from 'next/image'
+import GithubIcon from '../../icons/github'
+// import LogoIcon from '../../icons/logo'
+import mountainIcon from '../../icons/mountain.png'
+interface Props {
+  locale: string
+}
+export const Header: FC<Props> = ({ locale }) => {
+  const t = useTranslations('')
+  return (
+    <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
+      <Link lang={locale} href='/'>
+        <div className='flex flex-row items-center'>
+          <div className='mb-2 h-14 w-14'>
+            <Image
+              src={mountainIcon}
+              alt="Site Logo"
+            />
+          </div>
+          <strong className='mx-2 select-none'>Snowdonia Studio</strong>
+        </div>
+      </Link>
+      <div className='flex flex-row items-center gap-3'>
+        <nav className='mr-10 inline-flex gap-5'>
+          <Link lang={locale} href={`/about`}>
+            Meet The Team
+          </Link>
+          <a href=''>Contact Us</a>
+          <a href=''>Our Work</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,55 @@
+import { useTranslations } from 'next-intl'
+import Button from './components/Button'
+
+export default function DashboardPage() {
+  const t = useTranslations('')
+  return (
+    <div>
+      <section className='flex flex-col items-center justify-center py-24'>
+        <h1 className='text-center text-7xl font-extrabold leading-tight'>
+          
+          <span className='bg-span-bg bg-clip-text text-transparent'>
+            Lorem Ipsum
+          </span>
+          <br />
+          Lorem Ipsum Lorem Ipsum
+        </h1>
+        <div className='my-6 px-20 text-center text-2xl text-text-secondary'>
+            Lorem Ipsum Lorem Ipsum
+        </div>
+        <div className='mt-4 flex flex-row gap-4'>
+          <a
+            href=''
+            target='_blank'
+          >
+            <Button rounded size='large' variant='secondary'>
+              Work With Us
+            </Button>
+          </a>
+        </div>
+      </section>
+      <section className='bg-background-secondary py-20 max-lg:py-10'>
+        <div className='mx-auto grid max-w-screen-lg grid-cols-3 gap-7 px-8 py-5 max-lg:max-w-fit max-lg:grid-cols-1 max-lg:gap-10'>
+          <div className='text-center'>
+            <h2 className='mb-3  text-xl font-semibold'>Lorem Ipsum</h2>
+            <p className='text-text-secondary max-lg:max-w-[500px]'>
+            Lorem Ipsum Lorem Ipsum 
+            </p>
+          </div>
+          <div className='text-center'>
+            <h2 className='mb-3  text-xl font-semibold'>Lorem Ipsum</h2>
+            <p className='text-text-secondary max-lg:max-w-[500px]'>
+            Lorem Ipsum Lorem Ipsum
+            </p>
+          </div>
+          <div className='text-center'>
+            <h2 className='mb-3  text-xl font-semibold'>Lorem Ipsum</h2>
+            <p className='text-text-secondary max-lg:max-w-[500px]'>
+            Lorem Ipsum Lorem Ipsum
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
