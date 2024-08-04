320ffe355de1d1e887d8f4c61bc1b28487d34296
Jeremy Grice <jeremy.grice@newbalance.com>
2024-08-03 21:29:22 -0400

    Remove unescaped single quote

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -5,8 +5,8 @@ export default function ContactUsPage() {
     <div className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md ">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Need a website built? Need some maintenance or updates? Drop us a line and we'll get back with you as soon as possible.</p>
-            <form action="#" className="space-y-8">
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Need a website built? Need some maintenance or updates? Drop us a line and well get back with you as soon as possible.</p>
+            <form className="space-y-8">
                 <div>
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                     <textarea id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required> </textarea>
