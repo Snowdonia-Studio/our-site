8e8290a6553c969514cffd7809537dcb0ff8aade
Jeremy <jeremylgrice@gmail.com>
2024-08-09 11:55:23 -0400

    remove unescaped character

--- src/app/[locale]/work/page.tsx
+++ src/app/[locale]/work/page.tsx
@@ -9,7 +9,7 @@ export default function Work() {
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                 <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">What we've done</h2>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">What we have done</h2>
                 </div>
                 <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
                     <div className="relative max-w-xl mx-auto mt-20">
